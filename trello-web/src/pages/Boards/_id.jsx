3f963c73b500dcec39de4ad711c36f54b25d19cd
Long Nguyen <longwinpro113@gmail.com>
2025-10-15 16:34:22 +0700

    Calling APi using Axios

--- trello-web/src/pages/Boards/_id.jsx
+++ trello-web/src/pages/Boards/_id.jsx
@@ -1,16 +1,29 @@
 //? Board Details
+import { use, useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis/index'
 
 function Board() {
+    const [board, setBoard] = useState(null)  
+
+    useEffect(() => {
+        // Call API get board details
+        const boardID = '68ef6983f72c237870b0b692ai'
+
+        fetchBoardDetailsAPI(boardID).then(board => {
+            setBoard(board)
+        })
+    }, [])
+
     return (
         <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
             <AppBar />
-            <BoardBar board={mockData?.board} />
-            <BoardContent board={mockData?.board} />
+            <BoardBar board={board} />
+            <BoardContent board={board} />
         </Container>
     )
 }
